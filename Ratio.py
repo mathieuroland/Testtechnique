c5f155e8282a86d1a05d451e94747af083e5b38c
Mathieu <mathieu.roland@epita.Fr>
2023-04-18 19:07:58 +0200

    modif apiKey

--- Ratio.py
+++ Ratio.py
@@ -8,8 +8,9 @@ import json
 import urllib
 import urllib.request
 
+apiKey = "apiKey=e0a1bf2c844edb9084efc764c089dd748676cc14"
 #Récupération de la liste des villes
-res = urllib.request.urlopen("https://api.jcdecaux.com/vls/v3/contracts?apiKey=e0a1bf2c844edb9084efc764c089dd748676cc14").read()
+res = urllib.request.urlopen("https://api.jcdecaux.com/vls/v3/contracts?{}".format(apiKey)).read()
 #On récupéère les informations au format chaîne de caractères
 page = res.decode("utf8")
 #On les mets ensuites dans une liste pour y accéder plus facilement
@@ -17,7 +18,7 @@ js = json.loads(page)
 #On va parcourir la liste des villes
 for i in range(len(js)-1):
     #Récupération des stations de chaque ville
-    res_stat = urllib.request.urlopen("https://api.jcdecaux.com/vls/v3/stations?contract={}&apiKey=e0a1bf2c844edb9084efc764c089dd748676cc14".format(js[i]["name"])).read()
+    res_stat = urllib.request.urlopen("https://api.jcdecaux.com/vls/v3/stations?contract={}&{}".format(js[i]["name"],apiKey)).read()
     page_stat = res_stat.decode("utf8")
     js_stat = json.loads(page_stat)
     total_mec = 0
@@ -25,7 +26,7 @@ for i in range(len(js)-1):
     name = js[i]["name"]
     #Nous allons maintenant récupérer les informations de toutes le stations
     for j in range(len(js_stat)-1):
-        res_fin = urllib.request.urlopen("https://api.jcdecaux.com/vls/v3/stations/{}?contract={}&apiKey=e0a1bf2c844edb9084efc764c089dd748676cc14".format(js_stat[j]["number"],name)).read()
+        res_fin = urllib.request.urlopen("https://api.jcdecaux.com/vls/v3/stations/{}?contract={}&{}".format(js_stat[j]["number"],name,apiKey)).read()
         page_fin = res_fin.decode("utf8")
         js_fin = json.loads(page_fin)
         #On va récupérer le nombre de vélos électriques et mécaniques de chaque ville
